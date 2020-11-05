f64abb5946f70f6c2c0c78b472e1b9c55c72935b
ChiragJRana <42936626+ChiragJRana@users.noreply.github.com>
2020-11-05 11:17:58 +0530

    Fixed the bug of the float being passed to resize the image

--- flask/Application.py
+++ flask/Application.py
@@ -4,11 +4,12 @@ from PIL import ImageTk, Image
 import cv2
 import numpy as np
 from flask import Flask, request, jsonify
-from FaceMask import CustomerImage
-from alert import Alarm
+from .FaceMask import CustomerImage
+from .alert import Alarm
+from .imagesearch.centroidtracker import CentroidTracker
 import imutils
 import time
-import  threading
+import threading
 import datetime
 import os
 
@@ -16,7 +17,9 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 stat = False
 alarm = Alarm()
+# default password
 password = 'password'
+
 # Create a Product
 @app.route('/', methods=['POST'])
 def post_data():
@@ -68,16 +71,23 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self,parent)
 
         self.parent =parent
-        
         label = ttk.Label(self, text = "Welcome to the Face Mask Detection Application", font = LARGE_FONT)
-        label.pack(pady=15, padx=30)
-
+        label.pack(pady=15, padx=20)
+        self.entry = ttk.Entry(self)
+        self.entry.insert(10,password)    
+        self.entry.pack(padx=10, pady=20)
+        button2 = ttk.Button(self, text= "Change Password", command=self.changepassword)
+        button2.pack(side = tk.TOP, pady= 10)
         button1 = ttk.Button(self, text= "Start The Camera Recording", command=lambda: controller.show_frame(PageOne))
-        button1.pack()
+        button1.pack(side = tk.TOP, pady= 10)
         
+    def changepassword(self):
+        global password
+        password = self.entry.get()
+        print(password)
+
     def __del__(self):
         tk.Frame.__delattr__(self,self.parent)
-    
     
 
 class PageOne(tk.Frame):
@@ -92,38 +102,38 @@ class PageOne(tk.Frame):
         
         # ==================================== Widgets ======================================================  
         self.canvas = tk.Canvas(self, bg="black")
-        self.pic = Image.open("default.png")
-        self.frame = self.pic.copy()
+        self.default_image = Image.open("default.png")
+        self.frame = self.default_image
         self.width = 640
         self.height = 380
+
         self.canvas.configure(width = self.width, height = self.height)
         self.canvas.pack(side="top", fill="both", expand=True)
         self.canvas.bind('<Configure>', self._resize_image)
         button1 = ttk.Button(self, text= "Go back", command=lambda: controller.show_frame(StartPage))
-        button1.pack()
+        button1.pack(side = tk.RIGHT, padx = 10, pady = 10)
         self.StartButton = ttk.Button(self,text="Start The Recording", command= self.start_recording)
-        self.StartButton.pack()
+        self.StartButton.pack(side = tk.LEFT, padx = 10, pady = 10)
         
         # ========================== Additional   Variables =================================================
-        self.delay = 15
         self.limit = 0
         self.count = 0
         self.recording = False
         self.update()
         
     def start_recording(self):
-        print(self.recording)
+        # print(self.recording)
         if not self.recording:
             self.vid = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-            self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-            self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-            print(self.width, self.height)
+            # self.width = int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+            # self.height = int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            # print(self.width, self.height)
         
             if not self.vid.isOpened():
                 raise ValueError("Unable to open video source", 0)
             self.recording = True
             self.StartButton['text'] = "Stop The Recording"
-            print(self.StartButton['text'])
+            # print(self.StartButton['text'])
             self.update()
         else:
             self.recording = False
@@ -136,82 +146,78 @@ class PageOne(tk.Frame):
         
         if self.vid.isOpened():
             ret, frame = self.vid.read()
+
+        if ret:
             
-            if ret:
+            (locs, preds) = self.facemask.detect_and_find_face(frame)
             
-                (locs, preds) = self.facemask.detect_and_find_face(frame)
+            if len(preds) >= 3 and not self.alarm.alarm_switch and self.limit == 40:
+                    self.takeSnapShot()
+                    self.alarm.ring_alarm()
+                    self.limit = 0
+
+            for (box, pred) in zip(locs, preds):
+    
+                # unpack the bounding box and predictions
+                (startX, startY, endX, endY) = box
+                (mask, withoutMask) = pred
+
+                # determine the class label and color we'll use to draw the bounding box and text
+                label = "Mask" if mask > withoutMask else "No Mask"
+                color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
                 
-                if len(preds) >= 3 and not self.alarm.alarm_switch and self.limit == 40:
-                        self.takeSnapShot()
-                        self.alarm.ring_alarm()
-                        self.limit = 0
+                if label == 'Mask':
+                    self.limit = max(self.limit-1,0)
 
-                for (box, pred) in zip(locs, preds):
-        
-                    # unpack the bounding box and predictions
-                    (startX, startY, endX, endY) = box
-                    (mask, withoutMask) = pred
-
-                    # determine the class label and color we'll use to draw the bounding box and text
-                    label = "Mask" if mask > withoutMask else "No Mask"
-                    color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+                if label == "No Mask": 
+                    self.limit = min(40,self.limit + 1)
                     
-                    if label == 'Mask':
-                        self.limit = max(self.limit-1,0)
 
-                    if label == "No Mask": 
-                        self.limit = min(40,self.limit + 1)
-                        
+                if self.alarm.alarm_switch and self.limit == 0:
+                    self.alarm.stop_alarm()
 
-                    if self.alarm.alarm_switch and self.limit == 0:
-                        self.alarm.stop_alarm()
+                if not self.alarm.alarm_switch and  self.limit == 40:
+                    self.takeSnapShot()
+                    self.alarm.ring_alarm()
+                    self.limit = 0
+                    
+                # include the probability in the label
+                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-                    if not self.alarm.alarm_switch and  self.limit == 40:
-                        self.takeSnapShot()
-                        self.alarm.ring_alarm()
-                        self.limit = 0
-                        
-                    # include the probability in the label
-                    label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-                    # display the label and bounding box rectangle on the output frame
-                    cv2.putText(frame, label, (startX, startY - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                    cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-
-                # Return a boolean success flag and the current frame converted to BGR
-                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-            else:
-                return (ret, None)
-        else:
-            return (None, None)
+                # display the label and bounding box rectangle on the output frame
+                cv2.putText(frame, label, (startX, startY - 10),cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+            self.frame = frame
+            # Return a boolean success flag and the current frame converted to BGR
+            return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        
+        return (None, None)
     
     def _resize_image(self,event):
 
         self.width = event.width
         self.height = event.height
         print('New height:', self.height, 'new width:', self.width)
-        self.frame = self.frame.resize((self.width, self.height))
         if not self.recording:
-            self.pic = self.pic.resize((self.width, self.height))
-            self.photo = ImageTk.PhotoImage(self.pic.resize((self.width,self.height), Image.ANTIALIAS))
+            image_copy  = self.default_image.copy()
+            self.photo = ImageTk.PhotoImage(image_copy.resize((self.width,self.height), Image.ANTIALIAS))
             self.canvas.create_image(2, 2, image = self.photo, anchor ='nw')
 
     def update(self):
         if self.recording:
-            ret, self.frame = self.get_frame()
+            ret, frame = self.get_frame()
             if ret:
-                self.photo = ImageTk.PhotoImage(image = Image.fromarray(self.frame).resize((self.width,self.height), Image.ANTIALIAS))
+                self.photo = ImageTk.PhotoImage(image = Image.fromarray(frame).resize((self.width,self.height), Image.ANTIALIAS))
                 self.canvas.create_image(2, 2, image = self.photo, anchor ='nw')
-                self.after(1,self.update)
+                self.after(15,self.update)
         else:
-            self.frame = self.pic
-            self.photo = ImageTk.PhotoImage(Image.open("default.png").resize((self.width,self.height), Image.ANTIALIAS))
+            self.photo = ImageTk.PhotoImage(image =Image.open("default.png").resize((self.width,self.height), Image.ANTIALIAS))
             self.canvas.create_image(2, 2, image = self.photo, anchor ='nw')
 
     def takeSnapShot(self):
         ts = datetime.datetime.now()
         filename = "{}.jpg".format(ts.strftime("%Y-%m-%d`_%H-%M-%S"))         
-        cv2.imwrite('D:/SADproject/proofs/' + filename, cv2.cvtColor(np.float32(self.frame), cv2.COLOR_RGB2BGR))
+        cv2.imwrite('D:/SADproject/proofs/' + filename, cv2.cvtColor(self.frame, cv2.COLOR_RGB2BGR))
         
     def __del__(self):
         tk.Frame.__delattr__(self,self.parent)
